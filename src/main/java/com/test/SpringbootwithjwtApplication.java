7ce4049ab2201b7176f35a9b7389c7a6dae9040d
Arun Yadava <arunrnyada@gmail.com>
2018-09-19 12:17:20 +0530

    New user creation code

--- src/main/java/com/test/SpringbootwithjwtApplication.java
+++ src/main/java/com/test/SpringbootwithjwtApplication.java
@@ -1,12 +1,25 @@
 package com.test;
 
+import com.test.models.Account;
+import com.test.models.Role;
+import com.test.services.AccountService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.sql.DataSource;
+import java.util.Arrays;
+
 @SpringBootApplication
+@EnableAsync
 public class SpringbootwithjwtApplication {
 
 	public static void main(String[] args) {
@@ -17,5 +30,35 @@ public class SpringbootwithjwtApplication {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder(){
 		return new BCryptPasswordEncoder();
+	}
+
+
+	@Bean @Qualifier("mainDataSource")
+	public DataSource dataSource(){
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		EmbeddedDatabase db = builder
+				.setType(EmbeddedDatabaseType.H2)
+				.build();
+		return db;
+	}
+
+	@Bean
+	CommandLineRunner init(
+			AccountService accountService
+	) {
+		return (evt) -> Arrays.asList(
+				"user,admin,john,robert,ana".split(",")).forEach(
+				username -> {
+					Account acct = new Account();
+					acct.setUsername(username);
+					acct.setPassword("password");
+					acct.setFirstName(username);
+					acct.setLastName("LastName");
+					acct.grantAuthority(Role.ROLE_USER);
+					if (username.equals("admin"))
+						acct.grantAuthority(Role.ROLE_ADMIN);
+					accountService.registerUser(acct);
+				}
+		);
 	}
 }
